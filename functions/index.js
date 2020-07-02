2d9953808cd43877ccd9efa55ab8c902ca41c4bd
Mariana De Luna <marianadeluna9@gmail.com>
2020-07-02 08:38:40 -0700

    user login api

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,16 @@ const {
     editTodo
 } = require('./APIs/todos')
 
+const {
+    loginUser
+} = require('./APIs/users')
+
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
+
+//users
+app.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyANakD4YpttuWKHejQMnotRKIaloxdByrs",
+    authDomain: "to-do-app-7b339.firebaseapp.com",
+    databaseURL: "https://to-do-app-7b339.firebaseio.com",
+    projectId: "to-do-app-7b339",
+    storageBucket: "to-do-app-7b339.appspot.com",
+    messagingSenderId: "450992326249",
+    appId: "1:450992326249:web:75589c424657fa8536c4f8",
+    measurementId: "G-2CENZWJRBP"
+};
